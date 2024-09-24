0b28e0b3eeb50dbb27ed1d7395f67329a708349d
fersonaglio <sonaglio3@hotmail.com>
2024-09-24 20:21:54 -0300

    Updates jscript

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q="
 
   const searchBox = document.querySelector(".search input");
   const searchBtn = document.querySelector(".search button");
+  const weatherIcon = document.querySelector("weather-icon")
 
   async function checkWeather (city) {
     const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
@@ -15,6 +16,22 @@ const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q="
     document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°C";
     document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
     document.querySelector(".wind").innerHTML = data.wind.speed + " Km/h";
+
+    if (data.weather[0].main == "Clouds") {
+      weatherIcon.src = "images/clouds.png";
+    }
+    else if (data.weather[0].main == "Clear") {
+      weatherIcon.src = "images/clear.png";
+    }
+    else if (data.weather[0].main == "Rain") {
+      weatherIcon.src = "images/rain.png";
+    }
+    else if (data.weather[0].main == "Drizzle") {
+      weatherIcon.src = "images/drizzle.png";
+    }
+    else if (data.weather[0].main == "Mist") {
+      weatherIcon.src = "images/mist.png";
+    }
 
   }
 
